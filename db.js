9882b553632638cce5e0f7e17e0dcf7e0de67a8a
Ahmaad-Malik <ahmaadmalik01@gmail.com>
2026-01-10 17:13:59 +0500

    DB-Code

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose")
+
+const mongoURL='mongodb://localhost:27017/Social-Media-Platform';
+
+mongoose.connect(mongoURL);
+
+const db=mongoose.connection;
+
+db.on('connected', ()=>{console.log("Db is connected")})
+db.on('disconnected', ()=>{console.log("DB is disconnected")})
+db.on('error', (error)=>{console.log("Error occurred"+error)})
+
+module.exports=db;
