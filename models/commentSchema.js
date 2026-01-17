d455069370f4000ad6cde864ca4e620591dacbbc
wasiqaliiii <wasiq.ali.189@gmail.com>
2026-01-17 14:47:10 +0500

    comments

--- models/commentSchema.js
+++ models/commentSchema.js
@@ -1,96 +1,37 @@
-const express = require("express");
-const router = express.Router();
-const Comment = require("./commentSchema");
+const mongoose = require('mongoose')
 
-// write a comment
-router.post("/add", async (req, res) => {
-  try {
-    const { description, postId, userId } = req.body;
+const commentSchema = new mongoose.Schema(
+     {
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-    const comment = new Comment({
-      description,
-      postId,
-      userId,
-    });
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
 
-    await comment.save();
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
 
-    res.status(200).json(comment);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    collection: "Comment",
+    timestamps: true,
   }
-});
+);
 
-// see coments on a post
-router.get("/post/:postId", async (req, res) => {
-  try {
-    const id = req.params.postId;
-    const comments = await Comment.find({
-      postId: id,
-    });
-    if (!comments) {
-      res.status(404).json({ message: "No comments found!" });
-    }
-    res.json(comments);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// edit comment
-router.put("/update/:id", async (req, res) => {
-  try {
-    const pId = req.params.id;
-    const comment = await Comment.findById(pId);
-    if (!comment) {
-      return res.status(404).json({ message: "Comment not found" });
-    }
-
-    comment.description = req.body.description;
-    await comment.save();
-
-    res.status(200).json(comment);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// del a comment
-router.delete("/delete/:id", async (req, res) => {
-  try {
-    const cId = req.params.id;
-    const comment = await Comment.findById(cId);
-
-    if (!comment) {
-      return res.status(404).json({ message: "Comment not found" });
-    }
-
-    await comment.deleteOne();
-    res.status(200).json({ message: "Comment deleted" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-
-// like / unlike
-router.put("/like/:id", async (req, res) => {
-  try {
-    const { userId } = req.body;
-
-    const comment = await Comment.findById(req.params.id);
-
-    if (!comment.likes.includes(userId)) {
-      comment.likes.push(userId);
-    } else {
-      comment.likes.pull(userId);
-    }
-
-    await comment.save();
-    res.json(comment);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-module.exports = router;
+const Comment = mongoose.model('Comment',commentSchema);
+module.exports = Comment; 
