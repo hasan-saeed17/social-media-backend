1aaf6021bed2dfec94226b676842c4523d8a1a8c
Ahmaad-Malik <ahmaadmalik01@gmail.com>
2026-01-18 18:43:51 +0500

    sampleData

---
+++ sample-data/users.js
@@ -0,0 +1,53 @@
+// {
+//   "username": "admin01",
+//   "role": "admin",
+//   "name": "System Admin",
+//   "password": "admin@123",
+//   "profilePic": null,
+//   "gender": "male"
+// }
+// {
+//   "username": "john_doe",
+//   "role": "user",
+//   "name": "John Doe",
+//   "password": "john123",
+//   "email": "john.doe@example.com",
+//   "bio": "Tech enthusiast and sports lover",
+//   "profilePic": null,
+//   "gender": "male",
+//   "age": 25,
+//   "interests": [
+//     "tech",
+//     "sports"
+//   ]
+// }
+// {
+//   "username": "alice",
+//   "role": "user",
+//   "name": "Alice Smith",
+//   "password": "alice123",
+//   "email": "alice@example.com",
+//   "bio": "Fashion blogger",
+//   "profilePic": null,
+//   "gender": "female",
+//   "age": 22,
+//   "interests": [
+//     "fashion",
+//     "comedy"
+//   ]
+// }
+// {
+//   "username": "bob",
+//   "role": "user",
+//   "name": "Bob Lee",
+//   "password": "bob123",
+//   "email": "bob@example.com",
+//   "bio": "Startup founder",
+//   "profilePic": null,
+//   "gender": "male",
+//   "age": 28,
+//   "interests": [
+//     "business",
+//     "tech"
+//   ]
+// }
